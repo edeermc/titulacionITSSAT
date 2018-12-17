86d4531c565aa9d9c330a4abce00d47b16c1dcc6
Eder Morga <emc@sicemex.com>
2018-12-17 10:33:06 -0600

    Solicitud de examen

---
+++ app/Config/PDF.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eder Morga Camacho
+ * Date: 06/12/2018
+ * Time: 02:27 PM
+ */
+
+namespace App\Config;
+
+
+class PDF
+{
+
+}
---
+++ app/Controllers/ReporteController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eder Morga Camacho
+ * Date: 06/12/2018
+ * Time: 02:24 PM
+ */
+
+namespace App\Controllers;
+
+
+class ReporteController
+{
+
+}
